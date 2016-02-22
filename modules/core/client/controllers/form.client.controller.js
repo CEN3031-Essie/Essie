a9ccca4446d6247e7982ca1728cbab0223d50771
Ross Goldblum <rgoldblum84@gmail.com>
2016-02-22 11:31:39 -0500

    Upload buttons

--- modules/core/client/controllers/form.client.controller.js
+++ modules/core/client/controllers/form.client.controller.js
@@ -1,3 +1,15 @@
 'use strict';
 
-angular.module('core').controller('FormController', ['$scope', 'Authentication',function($scope, Authentication) {$scope.authentication = Authentication;$scope.forms = [{ code: 'aaa', name: 'test' },{ code: 'bbb', name: 'test2' }];}]);
+angular.module('core').controller('FormController', ['$scope', 'Authentication',
+	function($scope, Authentication) {
+		$scope.authentication = Authentication;
+		$scope.forms = [{ code: 'aaa', name: 'test' },
+						{ code: 'bbb', name: 'test2' }
+		];
+
+
+		// Changes XML to JSON
+		$scope.xmlToJson = function(xml) {
+		};
+	}
+]);
