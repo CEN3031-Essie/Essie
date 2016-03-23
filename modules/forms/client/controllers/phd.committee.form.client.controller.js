ff50460f7d26809b1d03ac8dcbb71bf4d1437a98
Gregory Wolfe <grwolfe123@gmail.com>
2016-03-23 02:13:45 -0400

    fixes bug in phd committee controller contecting to scope

--- modules/forms/client/controllers/phd.committee.form.client.controller.js
+++ modules/forms/client/controllers/phd.committee.form.client.controller.js
@@ -8,10 +8,10 @@
     PhdCommitteeFormController.$inject = ['$scope', '$state', 'Authentication'];
 
     function PhdCommitteeFormController($scope, $state, Authentication) {
-        function submitForm() {
+        $scope.submitForm = function () {
             // function to submit form information
             console.log('Form submitted');
             console.log($scope.form);
-        }
+        };
     }
 })();
