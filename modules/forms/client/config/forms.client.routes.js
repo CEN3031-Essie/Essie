f110df3023b0f6b2fd7b9d705fe9e4137f2f43df
Gregory Wolfe <grwolfe123@gmail.com>
2016-03-23 13:37:14 -0400

    fixes navbar hyperlink, adds state connection for plan of study form

--- modules/forms/client/config/forms.client.routes.js
+++ modules/forms/client/config/forms.client.routes.js
@@ -23,10 +23,15 @@
             url: '/success',
             templateUrl: 'modules/forms/client/views/form-submission-success.client.view.html'
         })
-        .state('forms.phd-form', {
+        .state('forms.phd-committee', {
             url: '/PhD-Program-Supervisory-Commitee',
             templateUrl: 'modules/forms/client/views/PHD-form.client.view.html',
             controller: 'PhdCommitteeFormController'
+        })
+        .state('forms.phd-plan-of-study', {
+            url: '/PhD-Plan-of-Study',
+            templateUrl: 'modules/forms/client/views/POS-form.client.view.html',
+            controller: 'PhdPlanOfStudyController'
         })
         .state('forms.key_request-form', {
             url: '/Key-Approval-and-Building-Access-Request',
