3213c1a4781fff626b24a0a66719e124db2bc868
Gregory Wolfe <grwolfe123@gmail.com>
2016-03-31 18:55:44 -0400

    change nomenclature [cleaner] and proper route changes

--- modules/forms/client/config/forms.client.routes.js
+++ modules/forms/client/config/forms.client.routes.js
@@ -21,22 +21,26 @@
         })
         .state('forms.success', {
             url: '/success',
-            templateUrl: 'modules/forms/client/views/form-submission-success.client.view.html'
+            templateUrl: 'modules/forms/client/views/form-success.client.view.html'
         })
         .state('forms.phd-committee', {
             url: '/PhD-Program-Supervisory-Commitee',
-            templateUrl: 'modules/forms/client/views/PHD-form.client.view.html',
+            templateUrl: 'modules/forms/client/views/phd-committee.client.view.html',
             controller: 'FormsController'
         })
         .state('forms.phd-plan-of-study', {
             url: '/PhD-Plan-of-Study',
-            templateUrl: 'modules/forms/client/views/POS-form.client.view.html',
+            templateUrl: 'modules/forms/client/views/phd-planOfStudy.client.view.html',
             controller: 'FormsController'
         })
-        .state('forms.key_request-form', {
+        .state('forms.key-request', {
             url: '/Key-Approval-and-Building-Access-Request',
-            templateUrl: 'modules/forms/client/views/Key_Request-form.client.view.html',
+            templateUrl: 'modules/forms/client/views/Key-Request.client.view.html',
             controller: 'FormsController'
+        })
+        .state('forms.', {
+            url: '/',
+            templateUrl
         });
     }
 })();
